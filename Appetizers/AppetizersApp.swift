dbb112f266d1127d8f7be75d1a2d525fbc90bd5f
aalpercem <aalpercem@gmail.com>
2023-09-21 11:34:06 +0300

    project creation: zero point

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Alper Cem Öztürk on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
