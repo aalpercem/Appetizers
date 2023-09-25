da112e7b018b869c250fb0a9c589e54f012b44fe
aalpercem <aalpercem@gmail.com>
2023-09-25 11:14:13 +0300

    feat: Add Appetizer model

---
+++ Networking/Sources/Networking/Resources/Models/AppetizerModel.swift
@@ -0,0 +1,38 @@
+//
+//  AppetizerModel.swift
+//
+//
+//  Created by Alper Cem Öztürk on 25.09.2023.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+  let id: String
+  let name: String
+  let decsription: String
+  let price: Double
+  let imageURL: String
+  let calories: Int
+  let protein: Int
+  let carbs: Int
+}
+
+struct AppetizerResponseDTO {
+  let request: [Appetizer]
+}
+
+struct MockData {
+  static let sampleAppetizer = Appetizer(
+    id: 1,
+    name: "Hot Dog",
+    decsription: "This is the description for my appetizer. Its yummy btw.",
+    price: 9.99,
+    mageURL: "",
+    calories: 280,
+    protein: 67,
+    carbs: 34
+  )
+  
+  static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
